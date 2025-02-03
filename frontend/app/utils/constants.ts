91b5eada39b1918e00b70510d21ca7047e6bd238
Mihai Chirculescu <mihai.chirculescu@yahoo.com>
2025-02-03 22:35:08 +0200

    Update constants.ts

--- frontend/app/utils/constants.ts
+++ frontend/app/utils/constants.ts
@@ -1,4 +1,4 @@
 export const baseURL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://mihaiii-actuosusai.hf.space:8000';
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:8000';
 export const wsURL =
-  process.env.NEXT_PUBLIC_WS_BASE_URL || 'ws://mihaiii-actuosusai.hf.space:8000';
+  process.env.NEXT_PUBLIC_WS_BASE_URL || 'ws://localhost:8000';
